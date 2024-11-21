106700ab26ba91743a589d3d9c71eadbe9acbe62
unknown <liashchenkomarina@gmal.com>
2024-11-21 23:07:17 +0100

    task

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,10 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
 
-createRoot(document.getElementById('root') as HTMLElement).render(
+createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
   </StrictMode>
 );
+
 
